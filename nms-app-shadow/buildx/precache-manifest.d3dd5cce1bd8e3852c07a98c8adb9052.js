5d7a6e71c8bf02dbaca6e73eaa77e85b2c679a05
Burrell <Steven.Burrell@coxautoinc.com>
2020-01-10 13:31:47 -0500

    add buildx files

---
+++ nms-app-shadow/buildx/precache-manifest.d3dd5cce1bd8e3852c07a98c8adb9052.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e27b96f8f977f0a8b0058e5aada3cd8d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "38c413d1ab3f97a82eba",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "3ec6d54c7d5627206bed",
+    "url": "/static/js/2.932d91ab.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.932d91ab.chunk.js.LICENSE"
+  },
+  {
+    "revision": "38c413d1ab3f97a82eba",
+    "url": "/static/js/main.27c298fd.chunk.js"
+  },
+  {
+    "revision": "5296af23fa1c693ccdc9",
+    "url": "/static/js/runtime-main.1bddea01.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
